9f2d28b92d0d0634407e02d30b1ac2d840b46605
minesettimi <minesettimi@gmail.com>
2026-08-27 22:33:56 -0400

    remove non-working locale option

--- Server/Loaders/PostLoad.cs
+++ Server/Loaders/PostLoad.cs
@@ -58,45 +58,5 @@ public class PostLoad(LocationService locationService, ReputationService reputat
         );
         
         imageRouterHelper.LoadFactionImages();
-        
-        //debug
-        templateTable.Quests["5a27b9de86f77464e5044585"].Rewards?["Success"].Add(new Reward
-        {
-            AvailableInGameEditions = [],
-            GameMode = ["regular", "pve"],
-            Id = new MongoId(),
-            IsHidden = false,
-            Type = (RewardType)150,
-            Unknown = false,
-            Target = "cultist",
-            Value = 0.3
-        });
-        
-        templateTable.Quests["5a27b9de86f77464e5044585"].Rewards?["Success"].Add(new Reward
-        {
-            AvailableInGameEditions = [],
-            GameMode = ["regular", "pve"],
-            Id = new MongoId(),
-            IsHidden = false,
-            Type = (RewardType)151,
-            Unknown = false,
-            Target = "cultist"
-        });
-        
-        templateTable.Quests["5a27b9de86f77464e5044585"].Conditions.AvailableForFinish?.Add(new QuestCondition
-        {
-            ConditionType = "Reputation",
-            DynamicLocale = false,
-            GlobalQuestCounterId = "",
-            Id = "69631a0bc6c5f245d93d25c2",
-            Index = 2,
-            OneSessionOnly = false,
-            IsEncoded = false,
-            ParentId = "",
-            Type = "TraderStanding",
-            Target = new ListOrT<string>(null, "cultist"),
-            Value = 7.5,
-            VisibilityConditions = []
-        });
     }
 }
